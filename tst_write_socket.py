6d8f5ff29036fe385bf05f4f6c2d0b86320057f7
Christian Zufferey <christian@zufferey.com>
2020-08-31 16:02:23 +0200

    Amélioration de mon test de write socket en python

--- tst_write_socket.py
+++ tst_write_socket.py
@@ -2,30 +2,33 @@
 # -*- coding: utf-8 -*-
 #
 # Petit script pour l'écriture dans un socket sur Internet
-# zf200831.1554
+# zf200831.1600
 
 import socket                
 
+print("début du programme...")
+
 s = socket.socket()    
 try:
-    print("connexion 1")
+    # print("connexion 1")
     s.connect(('www.zuzu-test.ml', 55514))
-    s.send(b'hello zuzu 1546.1\n') 
+    s.send(b'hello zuzu 1601.1\n') 
     s.close()   
 except socket.error as e:
-    print("oups..." + str(e))
+    pass
+    # print("oups..." + str(e))
     
     
 s = socket.socket()    
 try:
-    print("connexion 2")
+    # print("connexion 2")
     s.connect(('www.zuzu-test.ml', 55514))
-    s.send(b'hello zuzu 1546.2\n') 
+    s.send(b'hello zuzu 1601.2\n') 
     s.close()   
 except socket.error as e:
-    print("oups..." + str(e))
+    pass
+    # print("oups..." + str(e))
 
 
-print("toto")
-exit()
+print("suite du programme...")
 
