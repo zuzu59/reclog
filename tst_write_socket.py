b8b229b35a4c3d6882c7d986cc292c0b29fa1ee8
Christian Zufferey <christian@zufferey.com>
2020-08-31 18:01:34 +0200

    Dû améliorer la conversion en binaire d'un string. Maintenant avec Python 3, il est plus compliqué d'écrire un string dans un socket, on doit le faire en convertissant le string en bytes en indiquand quel format (UTF-8) on utilise.

--- tst_write_socket.py
+++ tst_write_socket.py
@@ -2,17 +2,18 @@
 # -*- coding: utf-8 -*-
 #
 # Petit script pour l'écriture dans un socket sur Internet
-# zf200831.1600
+# zf200831.1801
 
 import socket                
 
 print("début du programme...")
 
+ztimestamp = b'hello zuzu 1730.1\n'
 s = socket.socket()    
 try:
     # print("connexion 1")
     s.connect(('www.zuzu-test.ml', 55514))
-    s.send(b'hello zuzu 1601.1\n') 
+    s.send(bytes(str(ztimestamp), encoding='utf-8')) 
     s.close()   
 except socket.error as e:
     pass
